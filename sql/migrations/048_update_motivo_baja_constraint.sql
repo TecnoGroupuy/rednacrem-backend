83a1a66e485cc8ff4bafd4f16e73cce8ce738e5a
TecnoGroupuy <operaciones@globalassist.com.uy>
2026-06-06 10:01:40 -0300

    Update motivo baja constraint

---
+++ sql/migrations/048_update_motivo_baja_constraint.sql
@@ -0,0 +1,16 @@
+-- Migrate old motivo_baja values
+UPDATE contact_products SET motivo_baja = 'Auditoría' WHERE motivo_baja = 'no_pasa_auditoria';
+UPDATE contact_products SET motivo_baja = 'Voluntaria' WHERE motivo_baja = 'otro';
+
+-- Update check constraint
+ALTER TABLE contact_products
+DROP CONSTRAINT IF EXISTS contact_products_motivo_baja_check;
+
+ALTER TABLE contact_products
+ADD CONSTRAINT contact_products_motivo_baja_check
+CHECK (
+  motivo_baja IS NULL OR motivo_baja = ANY (ARRAY[
+    'Auditoría', 'Medio de pago', 'Voluntaria', 'Antel',
+    'BPS', 'Fallecido', 'Administrativa', 'Deuda'
+  ])
+);
