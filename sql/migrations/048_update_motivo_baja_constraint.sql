d6a08b4b4b4a3d0b0ee3adcadefb91b03e078aa9
TecnoGroupuy <operaciones@globalassist.com.uy>
2026-06-06 10:18:37 -0300

    Fix motivo baja migration order

--- sql/migrations/048_update_motivo_baja_constraint.sql
+++ sql/migrations/048_update_motivo_baja_constraint.sql
@@ -1,11 +1,11 @@
 -- Migrate old motivo_baja values
+ALTER TABLE contact_products
+DROP CONSTRAINT IF EXISTS contact_products_motivo_baja_check;
+
 UPDATE contact_products SET motivo_baja = 'Auditoría' WHERE motivo_baja = 'no_pasa_auditoria';
 UPDATE contact_products SET motivo_baja = 'Voluntaria' WHERE motivo_baja = 'otro';
 
 -- Update check constraint
-ALTER TABLE contact_products
-DROP CONSTRAINT IF EXISTS contact_products_motivo_baja_check;
-
 ALTER TABLE contact_products
 ADD CONSTRAINT contact_products_motivo_baja_check
 CHECK (
